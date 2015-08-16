f3845f15586fa48b972b078c9d3913ec78f2d0dc
Phil Freeman <paf31@cantab.net>
2015-08-16 11:16:24 -0700

    Minify

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ rm -f dist/*.js
 
 TIME=$(date +%s)
 
-sed -i '' 's/dist\/[0-9]*\.js/dist\/'$TIME'\.js/' index.html 
+sed -i '' 's/dist\/[0-9]*\.min\.js/dist\/'$TIME'\.min\.js/' index.html 
 
 pulp build && psc-bundle output/*/*.js --module Main > dist/${TIME}.js
+
+closure-compiler dist/${TIME}.js > dist/${TIME}.min.js
